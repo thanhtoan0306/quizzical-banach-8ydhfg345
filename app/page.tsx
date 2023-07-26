d2045d0dfca1be752a5031935e853c1153256d7e
Duong Thanh Toan <thanhtoan0306@gmail.com>
2023-07-26 17:24:42 +0000

    ju

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black items-center justify-between p-24">
-      <div className="text-white">Just a simple thing xL▌$æÑPÆ∟1@9Ñ</div>
+    <main className="flex text-white min-h-screen flex-col bg-black items-center justify-between p-24">
+      <div className="text-white">Just a simple thing </div>
+      <div>Chalk</div>
       <div>ahihi222222</div>
     </main>
   );
