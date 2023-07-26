589bc3632fa17288c0e0cb15cf92786eee9c33d3
Duong Thanh Toan <thanhtoan0306@gmail.com>
2023-07-26 17:09:47 +0000

    u2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-black items-center justify-between p-24">
       <div className="text-white">Just a simple thing xL▌$æÑPÆ∟1@9Ñ</div>
-      <div>ahihi</div>
+      <div>ahihi222222</div>
     </main>
   );
 }
